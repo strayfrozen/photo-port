cd6964b772da8b70724e34c91c869d8d7b7e04d9
Stray <strayfrozen@yahoo.com>
2022-02-21 16:19:45 -0600

    wrote four more tests

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -17,8 +17,24 @@ describe('About component', () => {
         const { asFragment } = render(<About />);
 
         expect(asFragment()).toMatchSnapshot();
-      })
+      });
   
     // Second Test
     
+  })
+
+  describe('emoji is visible', () => {
+    it('inserts emoji into the h2', () => {
+    const { getByLabelText } = render(<Nav />);
+  
+    expect(getByLabelText('camera')).toHaveTextContent('📸');
+    });
+  })  
+
+  describe('links are visible', () => {
+    it('inserts text into the links', () => {
+      const { getByTestId } = render(<Nav />);
+      expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
+      expect(getByTestId('about')).toHaveTextContent('About me');
+    });
   })

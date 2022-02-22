fbaf7adff7c2720d91858589a576b772d612131d
Stray <strayfrozen@yahoo.com>
2022-02-21 22:19:47 -0600

    added gallery

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import React, { useState } from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
@@ -25,10 +24,8 @@ function App() {
         currentCategory={currentCategory}
       ></Nav>
       <main>
-        <div>
-          <Gallery></Gallery>
-          <About></About>
-        </div>
+        <Gallery currentCategory={currentCategory}></Gallery>
+        <About></About>
       </main>
     </div>
   );
--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -1,40 +1,19 @@
+// __tests__/About.test.js
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import About from '..';
 
-
 afterEach(cleanup);
 
-describe('About component', () => {
-    // First Test
-    it('renders', () => {
-      render(<About />);
-      
-    });
-    it('matches snapshot DOM node structure', () => {
-        // render About
-        const { asFragment } = render(<About />);
-
-        expect(asFragment()).toMatchSnapshot();
-      });
+describe('About component renders', () => {
+  it('renders', () => {
+    render(<About />);
+  });
   
-    // Second Test
+  it('matches snapshot DOM node structure', () => {
+    const { asFragment } = render(<About />);
     
-  })
-
-  describe('emoji is visible', () => {
-    it('inserts emoji into the h2', () => {
-    const { getByLabelText } = render(<Nav />);
-  
-    expect(getByLabelText('camera')).toHaveTextContent('📸');
-    });
-  })  
-
-  describe('links are visible', () => {
-    it('inserts text into the links', () => {
-      const { getByTestId } = render(<Nav />);
-      expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
-      expect(getByTestId('about')).toHaveTextContent('About me');
-    });
-  })
+    expect(asFragment()).toMatchSnapshot();
+  });
+})
